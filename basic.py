5a659a21983efc61687c4f20494ec70838f73f07
Your Name <your_email@example.com>
2025-08-19 21:27:05 +0600

    third commit

---
+++ basic.py
@@ -0,0 +1,72 @@
+#concantinaton
+
+"""
+text= ("Two AND Two MAKES ")
+
+math=2+2
+result= str (math)
+
+print(text + result)
+"""
+
+#if else statement
+"""""
+age= 19
+
+if (age<=13):
+    print("you are an kid")
+elif (age<=18):
+    print("you are a teen ager")
+else:
+    print("You are a adult")
+"""
+
+#loop statement
+#while (condition)
+       #code 
+ 
+       #conditon
+"""""       
+i=1
+while i<=100:
+       if i%2 == 0:
+              print(i)
+       i = i + 1
+       """
+
+#list and for loop
+"""""
+data=[2, 3, 4, 5, 7, 10]
+i=0
+while i<=5:
+    print(data [i])
+    i = i+1
+    for loop
+data=[2, 3, 4, 5, 7, 10,12]
+
+for x in data:
+    print(x)
+    
+    
+    """
+
+#function def is function keyword
+"""""
+def simpleFunction():
+    print("say hello to function")
+    a,b = 2,4
+    sum =a + b
+    print(sum)
+simpleFunction()
+"""
+#function peramiter
+"""""
+def sum(a,b):
+    sum= a+b
+    print(sum)
+sum(2,4)
+sum(10,50)
+"""
+#input from user
+name= input ("Enter your name ")
+print("Hello "+name)
